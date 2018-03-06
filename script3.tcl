29a144cd118ee0333af27fe2ec14dbe793ed4859
Cristian Guadalupe Santana Arriaga <crisgpe12dic@gmail.com>
2018-03-06 01:10:12 -0600

    Add files via upload

--- script3.tcl
+++ script3.tcl
@@ -1,45 +1,47 @@
 set ns [new Simulator]
+#Define different colors for data flows (for NAM)
 $ns color 1 Blue
 $ns color 2 Red
-
-set file1 [open out3.tr w]
+#Open the trace files
+set file1 [open out.tr w]
 set winfile [open WinFile w]
 $ns trace-all $file1
-
-set file2 [open out3.nam w]
-$ns trace-all $file2
-
+#Open the NAM trace file
+set file2 [open out.nam w]
+$ns namtrace-all $file2
+#Define a ’finish’ procedure
 proc finish {} {
-	global ns file1 file2
+	lobal ns file1 file2
 	$ns flush-trace
 	close $file1
 	close $file2
-	exec nam out3.nam &
+	exec nam out.nam &
 	exit 0
 }
-
+#Create six nodes
 set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
-
+#Create links between the nodes
 $ns duplex-link $n0 $n2 2Mb 10ms DropTail
 $ns duplex-link $n1 $n2 2Mb 10ms DropTail
 $ns simplex-link $n2 $n3 0.3Mb 100ms DropTail
 $ns simplex-link $n3 $n2 0.3Mb 100ms DropTail
 $ns duplex-link $n3 $n4 0.5Mb 40ms DropTail
 $ns duplex-link $n3 $n5 0.5Mb 30ms DropTail
-
+#Give node position (for NAM)
 $ns duplex-link-op $n0 $n2 orient right-down
 $ns duplex-link-op $n1 $n2 orient right-up
 $ns simplex-link-op $n2 $n3 orient right
 $ns simplex-link-op $n3 $n2 orient left
 $ns duplex-link-op $n3 $n4 orient right-up
 $ns duplex-link-op $n3 $n5 orient right-down
-$ns queue-limit  $n2 $n3 20
-
+#Set Queue Size of link (n2-n3) to 10
+$ns queue-limit $n2 $n3 20
+#Set up a TCP connection
 set tcp [new Agent/TCP/Newreno]
 $ns attach-agent $n0 $tcp
 set sink [new Agent/TCPSink/DelAck]
@@ -48,18 +50,18 @@ $ns connect $tcp $sink
 $tcp set fid_ 1
 $tcp set window_ 8000
 $tcp set packetSize_ 552
-
+#Setup an FTP application over the TCP connection
 set ftp [new Application/FTP]
 $ftp attach-agent $tcp
-$ftp set type_  FTP
-
+$ftp set type_ FTP
+#Setup a UDP flow
 set udp [new Agent/UDP]
 $ns attach-agent $n1 $udp
 set null [new Agent/Null]
 $ns attach-agent $n5 $null
 $ns connect $udp $null
-$ns set fid_ 2
-
+$udp set fid_ 2
+#Setup a CBR traffic source over the UDP flow
 set cbr [new Application/Traffic/CBR]
 $cbr attach-agent $udp
 $cbr set type_ CBR
@@ -70,7 +72,8 @@ $ns at 0.1 "$cbr start"
 $ns at 1.0 "$ftp start"
 $ns at 124.0 "$ftp stop"
 $ns at 124.5 "$cbr stop"
-
+# next procedure gets two arguments: the name of the
+# tcp source node ("tcp") and the name of output file.
 proc plotWindow {tcpSource file} {
 	global ns
 	set time 0.1
@@ -80,11 +83,8 @@ proc plotWindow {tcpSource file} {
 	puts $file "$now $cwnd"
 	$ns at [expr $now+$time] "plotWindow $tcpSource $file"
 }
-
 $ns at 0.1 "plotWindow $tcp $winfile"
-$ns at 125.0 "finish"
+
+ns at 125.0 "finish"
 $ns run
-
-
-
 
